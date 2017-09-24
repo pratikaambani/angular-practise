909018c9fd3f99a9935f6962a3a7496ec9f0080d
Pratik <pratikaambani@gmail.com>
2017-09-24 19:30:08 +0530

    added $scope

--- scripts/blog.list.component.js
+++ scripts/blog.list.component.js
@@ -1,6 +1,7 @@
 'user strict';
 
 angular.module('customModule')
-	.controller('blogListController', function() {
+	.controller('blogListController', function($scope) {
 		console.log("Hellowwww from component ;)");
+		$scope.title = "Hi, from scope"
 	});
