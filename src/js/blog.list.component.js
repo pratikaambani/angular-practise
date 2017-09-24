17bd23a89c6cd35285779cee7e7a4a44bb326698
Pratik <pratikaambani@gmail.com>
2017-09-25 00:16:24 +0530

    007 added external template

--- src/js/blog.list.component.js
+++ src/js/blog.list.component.js
@@ -15,8 +15,9 @@ angular.module('customModule')
 //006
 angular.module('myModule')
 	.component('customModule', {
-		template: "<div class=''><h1>{{title}}</h1><button ng-click='onBtnClick()'>Click me!!</button></div>",
-//		templateUrl:'scripts/templates/blog-list.html',
+//		template: "<div class=''><h1>{{title}}</h1><button ng-click='onBtnClick()'>Click me!!</button></div>",
+//007 External Template
+		templateUrl:'js/templates/blog-list.html',
 		controller: function($scope) {
 			$scope.title = 'Hi, from scope'
 			$scope.onBtnClick = function() {
