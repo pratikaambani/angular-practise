1c2b6a5bb16c4eb0b96158ca24537aa363e373f9
Pratik <pratikaambani@gmail.com>
2017-09-25 03:20:12 +0530

    added 008-01 To remove # from url | location provider in html

--- src/js/app.config.js
+++ src/js/app.config.js
@@ -9,9 +9,17 @@ angular.module('customModule')
 // 008 
 angular.module('customModule')
 	.config( function($routeProvider, $locationProvider) {
+		$locationProvider.html5Mode({
+			enabled:true,
+			requireBase: false
+		})
+
+
 		$routeProvider
 			.when("/blog/1", {
 				template: "<h1>Hiiiiiiiiiiiii</h1>"
+			}).when("/", {
+				template: "<custom-module></custom-module>"
 			}).when("/blog/2", {
 				template: "Not Found"
 			}).otherwise({
