e5e5db81b599636a7a8b41409311d3ce446cae2e
Pratik <pratikaambani@gmail.com>
2017-09-24 19:52:44 +0530

    tweaking scope

--- scripts/blog.list.component.js
+++ scripts/blog.list.component.js
@@ -2,6 +2,11 @@
 
 angular.module('customModule')
 	.controller('blogListController', function($scope) {
-		console.log("Hellowwww from component ;)");
-		$scope.title = "Hi, from scope"
+		console.log("Hellowwww from component")
+		$scope.title = 'Hi, from scope'
+		$scope.onBtnClick = function() {
+			alert("Clicked re");
+			console.log("Button clicked"),
+			$scope.title = "Clicked ;)"
+		}
 	});
